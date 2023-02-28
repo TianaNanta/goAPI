d114dacdb84925a71ce8edfe1f9701ea81674dac
TianaNanta <andrianjakananta@gmail.com>
2023-02-28 11:43:38 +0300

    update models for user

--- models/userModel.go
+++ models/userModel.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username string `json:"username" binding:"required" gorm:"unique"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Avatar   string `json:"avatar"`
+	Email    string `json:"email" null:"true"`
+	Password string `json:"password" null:"true"`
+	Avatar   string `json:"avatar" null:"true"`
 }
